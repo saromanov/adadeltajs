2d66d1a5af650e3dbe3452de3ab037e2493d0abf
Sergey Romanov <xxsmotur@gmail.com>
2015-09-20 03:41:19 +0500

    Update

--- src/adadeltajs.js
+++ src/adadeltajs.js
@@ -1,4 +1,5 @@
 import mathjs, {multiply, add, divide, subtract, dot, sum} from 'mathjs';
+import random from 'random-array';
 
 
 export class Adadeltajs {
@@ -7,14 +8,19 @@ export class Adadeltajs {
         this.y = y;
     }
 
-    fit(iters=100, rho=0.001) {
+    fit(iters=100, rho=0.001, eps=0.001) {
         let converge = false;
+        let theta0 = random(-0.001, 0.001).oned(this.X.length);
         while(!converge) {
             let value = multiply(this.X, theta0);
             let loss = subtract(value, this.y);
             let J = sum(multiply(loss, loss));
             let grad = divide(multiply(this.X, loss), m);
             let accgrad = add(multiply(grad, rho), multiply(multiply(grad, grad), (1 - rho)));
+            theta0 = theta0 + deltax;
+            
         }
+
+        return theta0;
     }
 }
