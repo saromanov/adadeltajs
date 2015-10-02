3409e0c2f96cf2fb0fc2243696b78de99392c633
Sergey Romanov <xxsmotur@gmail.com>
2015-10-02 22:01:12 +0500

    Update model

--- src/adadeltajs.js
+++ src/adadeltajs.js
@@ -19,8 +19,8 @@ export class Adadeltajs {
             let J = sum(multiply(loss, loss));
             let grad = divide(multiply(this.X, loss), m);
             let accgrad = add(multiply(grad, rho), multiply(multiply(grad, grad), (1 - rho)));
-            let step = multiply(div(sqrt(accvalue + offset), sqrt(accgrad + offset)), grad);
-            theta0 = add(theta0, deltax);
+            let step = multiply(-1, multiply(div(sqrt(accvalue + offset), sqrt(accgrad + offset)), grad));
+            theta0 = add(theta0, step);
             
         }
 
